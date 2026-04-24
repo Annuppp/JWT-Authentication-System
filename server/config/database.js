3c898fb2fbc40354570b5374af25f44c05d12a11
Anup Bhattachan <anupbhattachan10@gmail.com>
2026-04-24 10:30:07 +0545

    deleted everything in server folder to setup again

--- server/config/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import config from "./config.js";
-
-const connectDB = async () => {
-    mongoose.connect(config.MONGO_URI);
-    console.log("Database connected");
-};
-
-export default connectDB;
--- server/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-import connectDB from "./config/database.js";
-
-connectDB();
-
-app.listen(3000, () => {
-    console.log("Server has been connected to port 3000");
-});
