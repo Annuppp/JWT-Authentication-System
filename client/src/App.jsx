367f90573693b85438885a095b4fcf21de87134f
Anup Bhattachan <anupbhattachan10@gmail.com>
2026-04-27 10:40:33 +0545

    deleted imports as well

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,3 @@
 import "./App.css";
-import { Newsletter } from "./Newsletter.jsx";
-import { Contact } from "./Contact.jsx";
-import { CustomButton } from "./customButton.jsx";
 
 export default App;
