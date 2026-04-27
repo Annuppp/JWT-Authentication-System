809d73034d22faddcdbaef5231159c304bb2cf76
Anup Bhattachan <anupbhattachan10@gmail.com>
2026-04-27 10:40:12 +0545

    cleared and deleted every file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,4 @@ import { Newsletter } from "./Newsletter.jsx";
 import { Contact } from "./Contact.jsx";
 import { CustomButton } from "./customButton.jsx";
 
-const App = () => {
-    return (
-        <div>
-            <Contact />
-            <Newsletter />
-            <CustomButton text="Custom" />
-        </div>
-    );
-};
-
 export default App;
