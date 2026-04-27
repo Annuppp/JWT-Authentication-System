ad741bd33df834231285ddd18d70b7dc09d73b7a
Anup Bhattachan <anupbhattachan10@gmail.com>
2026-04-27 16:42:01 +0545

    made register route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,9 @@
 import Router from "express";
+import * as authController from "../controller/auth.controller.js";
 
 const authRouter = Router();
+
+// register user
+authRouter.post("/", authController.userRegister);
 
 export default authRouter;
