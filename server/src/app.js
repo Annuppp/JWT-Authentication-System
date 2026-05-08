6198ad95bd6709ff1c46a1c0fbd30a1d714ac5fc
Anup Bhattachan <anupbhattachan10@gmail.com>
2026-05-08 20:16:36 +0545

    stored refresh token inside of cookie

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
-app.unsubscribe(cookieParser());
+app.use(cookieParser());
 
 // routes
 app.use("/api/auth", authRouter);
