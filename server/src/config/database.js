d7c1543447f84264bc41290166c286ae5e3db0c1
Anup Bhattachan <anupbhattachan10@gmail.com>
2026-05-15 16:09:33 +0545

    cleared everything

--- server/src/config/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import config from "./config.js";
-
-const connectDB = () => {
-    mongoose.connect(config.MONGO_URI);
-    console.log("Database Connected");
-};
-
-export default connectDB;
